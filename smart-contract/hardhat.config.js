516d256b54034daa8462f5836c34a2d9e8955af0
Mohit Vashistha <mohitvashistha@Mohits-MacBook-Pro.local>
2023-03-26 12:00:32 +0530

    Added support for Scroll, Optimism and Gnosis chain

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -23,6 +23,22 @@ allowUnlimitedContractSize: 'true',
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [POLYGON_PRIVATE_KEY]
+    },
+    gnosis: { //Need xDAI
+      url: "https://rpc.gnosischain.com",
+      accounts: [GNOSIS_PRIVATE_KEY]
+    },
+    scroll_alpha_testnet : {
+      url: "https://alpha-rpc.scroll.io/l2",
+      accounts: [SCROLL_PRIVATE_KEY]
+    },
+    scroll_georli_testnet: {
+      url: "https://endpoints.omniatech.io/v1/eth/goerli/public",
+      accounts: [SCROLL_PRIVATE_KEY]
+    },
+    georli_optimism_testnet: {
+      url: "https://goerli.optimism.io",
+      accounts: [OPTIMISM_PRIVATE_KEY]
     }
   },
   etherscan: {
